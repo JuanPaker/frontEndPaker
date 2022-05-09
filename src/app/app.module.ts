01a1b7dad761d6b06db186063d591f508dcb6a4e
JuanPaker <95170654+JuanPaker@users.noreply.github.com>
2022-05-09 08:46:51 -0300

    Carga proyecto avanzado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import 'animate.css';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { AptitudesComponent } from './components/aptitudes/aptitudes.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { ExperLaboralComponent } from './components/exper-laboral/exper-laboral.component';
+import { EncabezadoComponent } from './components/encabezado/encabezado.component';
+import { InicioSesionComponent } from './components/inicio-sesion/inicio-sesion.component';
+import { LogoArgProgComponent } from './components/logo-arg-prog/logo-arg-prog.component';
+import { PorfolioComponent } from './components/porfolio/porfolio.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { RedesSocialesComponent } from './components/redes-sociales/redes-sociales.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AcercaDeComponent,
+    AptitudesComponent,
+    BannerComponent,
+    EducacionComponent,
+    ExperLaboralComponent,
+    EncabezadoComponent,
+    InicioSesionComponent,
+    LogoArgProgComponent,
+    PorfolioComponent,
+    ProyectosComponent,
+    RedesSocialesComponent
   ],
   imports: [
     BrowserModule,
