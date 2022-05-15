29940838dda02f39cf17346647b14ba4ccf5c435
Juan Paker <95170654+JuanPaker@users.noreply.github.com>
2022-05-14 21:11:30 -0300

    carga carpeta services

---
+++ src/app/services/porfolio.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, observable } from 'rxjs';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class PorfolioService {
+ // url:string="localhost:8080/new/persona";
+  constructor(private http:HttpClient) { }
+
+  obtenerDatos(): Observable<any>
+  {
+    //return this.http.get<any>(this.url+"persona");
+    return this.http.get<any>('./assets/data.json');
+  }
+
+}
+
+
